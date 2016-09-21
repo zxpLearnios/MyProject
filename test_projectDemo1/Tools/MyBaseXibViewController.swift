1b2d75eb674a60eaf56b7348bc9d7bbf4fc57167
zjn <zxp1991tqd@163.com>
2016-09-21 17:08:36 +0800

    新增无须写init方法的baseViewController

---
+++ test_projectDemo1/Tools/MyBaseXibViewController.swift
@@ -0,0 +1,25 @@
+//
+//  QLBaseXibViewController.swift
+//  QTimelyLoan
+//
+//  Created by Jingnan Zhang on 16/9/21.
+//  Copyright © 2016年 Jingnan Zhang. All rights reserved.
+//  1. 所有通过xib创建的控制器，xib名必须与类名一致， 继承之之后无须在写convenience方法加载xib了
+//  2. TVC用xib不能编辑，故没意义的。
+
+
+import UIKit
+
+class  MyBaseXibViewController:UIViewController {
+    
+   
+    convenience init(){
+        let type = NSStringFromClass(self.dynamicType)
+        let name = type.componentsSeparatedByString(".").last!
+        self.init(nibName: name, bundle: nil)
+        
+    }
+    
+    
+    
+}
